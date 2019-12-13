844a9c5e7b22f515f06015f545ffe5fd29b71969
stvansolano <stvansolano@outlook.com>
2019-12-12 19:34:31 -0600

    Sesion de Ciclos e iteraciones

---
+++ sesiones/CiclosIteraciones/Notas.md
@@ -0,0 +1,8 @@
+
+
+
+dotnet new (Nuevo proyecto)
+dotnet build (Compila)
+dotnet run (correr)
+
+Tarea: dotnet new web
---
+++ sesiones/CiclosIteraciones/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for( i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Hola! {i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
