d7b031b33fe487a654057e1d476f64da4c926cab
stvansolano <stvansolano@outlook.com>
2019-12-12 18:17:38 -0600

    Material Sesion 03

---
+++ sesiones/Condicionales/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Condicionales");
+            Console.WriteLine("Es usted estudiante?");
+
+            var cadena = Console.ReadLine();
+
+            Console.WriteLine("Usted escribio: " + cadena);
+
+            // 1) Descarte
+            // 2) funciones "string"
+            // 3) Expresiones regulares o RegEx
+
+            bool evaluacion = cadena == "si" // OR
+                             || cadena.ToUpper() ==  "SI";
+
+            if (evaluacion)
+            {
+                Console.WriteLine("Ud. es estudiante!");
+            }
+            else 
+            {
+                Console.WriteLine("Ud NO es estudiante");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
